8f864f0cb48d8de3d9afb35625f14ee4b32d0428
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-07-20 23:29:18 +0530

    ch 14.2

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,49 +1,20 @@
 // accept command line args
-use std::{env, fs, process};
-
+use std::{env, process};
+use cmd::Config;
 
 fn main() {
-    // first value in the vecotr is binary then all the other passed args. 
+    // first value in the vector is binary then all the other passed args. 
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-   
     // takes the file path, open that file, and returns a value of type std::io::Result<String>
     // that contains file's contents
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+    if let Err(e) = cmd::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
-
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
-//
-//     Config { query, file_path }
-// }
