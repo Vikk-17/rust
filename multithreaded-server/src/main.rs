db96fdf80dc0a6a6f3d5538b062aadc640b2e63c
hipster0x <Souvikc3030@gmail.com>
2025-07-31 19:40:45 +0530

    server

---
+++ multithreaded-server/src/main.rs
@@ -0,0 +1,18 @@
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{BufRead, prelude::*},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // ...
+}
