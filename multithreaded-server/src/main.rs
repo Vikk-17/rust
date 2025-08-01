f054d9b2ea10b0167979b40b3535ec0043e9302a
hipster0x <Souvikc3030@gmail.com>
2025-08-01 21:42:19 +0530

    single thread

--- multithreaded-server/src/main.rs
+++ multithreaded-server/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
+    fs,
+    io::{prelude::*, BufRead, BufReader},
     net::{TcpListener, TcpStream},
-    io::{BufRead, prelude::*},
 };
 
 fn main() {
@@ -14,5 +15,17 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // ...
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+   
+    let(status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\rn\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
