d83d52a592b00f4b698fd61a23bf496a78bbd35e
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-10-06 15:56:43 +0530

    signle thread

---
+++ web_server/src/main.rs
@@ -0,0 +1,75 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
+};
+
+
+fn handle_client(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // // println!("The http_request: {http_request:?}");
+    // if request_line == "GET / HTTP/1.1" {
+    //     let contents = fs::read_to_string("hello.html").unwrap();
+    //     let contents_len = contents.len();
+    //     let status_line = "HTTP/1.1 200 OK";
+    //     let response = format!("Status line: {status_line}\r\nLenght: {contents_len}\r\n\r\nContents: {contents}");
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // } else {
+    //     let contents = fs::read_to_string("404.html").unwrap();
+    //     let contents_len = contents.len();
+    //     let status_line = "HTTP/1.1 404 NOT FOUND";
+    //     let response = format!("Status line: {status_line}\r\nLenght: {contents_len}\r\n\r\nContents: {contents}");
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // }
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT OK", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let contents_len = contents.len();
+   let response = format!("Status line: {status_line}\r\nLenght: {contents_len}\r\n\r\nContents: {contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+
+}
+
+
+fn main() -> std::io::Result<()> {
+
+    // what if I need multiple address
+    // let addrs = [
+    //     SocketAddr::from(([127, 0, 0, 1], 80)),
+    //     SocketAddr::from(([127, 0, 0, 1], 443)),
+    // ];
+
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    // match listener.accept() {
+    //     Ok((_socket, addr)) => println!("new client {addr:?}"),
+    //     Err(e) => println!("Error {e:?}"),
+    // }
+    // for stream in listener.incoming() {
+    //     // println!("{:?}", stream);
+    //     match stream {
+    //         Ok(stream) => println!("Ok {:?}", stream),
+    //         Err(e) => println!("Error {:?}", e),
+    //     }
+    // }
+
+    // println!("{:?}", listener.local_addr()?);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream)=> handle_client(stream),
+            Err(e) => eprintln!("Error: {e:?}"),
+        }
+    }
+    Ok(())
+}
