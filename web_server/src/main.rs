d67c59771faaa8dce7ae27d3d4e5c5ace962ef1b
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-10-08 00:03:15 +0530

    update

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,5 +1,8 @@
+use web_server::ThreadPool;
 use std::{
     fs,
+    thread,
+    // time::Duration,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream}
 };
@@ -33,10 +36,22 @@ fn handle_client(mut stream: TcpStream) {
     } else {
         ("HTTP/1.1 404 NOT OK", "404.html")
     };
+
+
+    // simulating latency => blocking the thread by using sleep calls
+    // let (status_line, filename) = match &request_line[..] {
+    //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+    //     "GET /sleep HTTP/1.1" => {
+    //         thread::sleep(Duration::from_secs(5));
+    //         ("HTTP/1.1 200 OK", "hello.html")
+    //     }
+    //     _ => ("HTTP/1.1 404 NOT OK", "404.html"),
+    // };
+
     let contents = fs::read_to_string(filename).unwrap();
     let contents_len = contents.len();
-   let response = format!("Status line: {status_line}\r\nLenght: {contents_len}\r\n\r\nContents: {contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    let response = format!("Status line: {status_line}\r\nLenght: {contents_len}\r\n\r\nContents: {contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 
 }
 
@@ -51,6 +66,8 @@ fn main() -> std::io::Result<()> {
 
     let listener = TcpListener::bind("127.0.0.1:3000")?;
 
+    // create a pool of 4 threads
+    let pool = ThreadPool::new(4);
     // match listener.accept() {
     //     Ok((_socket, addr)) => println!("new client {addr:?}"),
     //     Err(e) => println!("Error {e:?}"),
@@ -67,7 +84,11 @@ fn main() -> std::io::Result<()> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream)=> handle_client(stream),
+            Ok(stream)=> {
+                pool.execute(||{
+                    handle_client(stream)
+                }) 
+            },
             Err(e) => eprintln!("Error: {e:?}"),
         }
     }
