701c396c623b1c5ea994009ada17fe4a3882a33b
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-10-16 01:16:23 +0530

    update

--- chapter_4/slicing/src/main.rs
+++ chapter_4/slicing/src/main.rs
@@ -1,18 +1,38 @@
+// fn main() {
+//     let mut s = String::from("Rust Boy");
+//     let len = first_word(&s); 
+//     println!("{len}");
+//     s.clear();
+// }
+//
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+//
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+//
+//     s.len()
+// }
+
+
+
+
 fn main() {
-    let mut s = String::from("Rust Boy");
-    let len = first_word(&s); 
-    println!("{len}");
-    s.clear();
+   let s = String::from("Rusty Boy");
+   let len = part_of_words(&s);
+   println!("{len:?}");
 }
 
-fn first_word(s: &String) -> usize {
+fn part_of_words(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
+        if item == "0x32" {
             return i;
         }
     }
-
     s.len()
 }
